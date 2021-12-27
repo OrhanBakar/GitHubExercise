38958fc0c61e76126ace295cb22cf88542a55202
OrhanBakar <obkr4206@gmail.com>
2021-12-27 18:19:15 +0100

    Added more work

--- src/exercise1/Hello.java
+++ src/exercise1/Hello.java
@@ -4,6 +4,8 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello Orhan! I hope you understand GitHub");
+        System.out.println("We created Git repository");
+        System.out.println("Added");
     }
 
 
